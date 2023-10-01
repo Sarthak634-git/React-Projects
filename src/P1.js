85c30febc228a3c47e25cab69a33e24dbe5b175d
Sarthak Thapa <sarthakthapa634@gmail.com>
2023-10-01 21:22:11 +0545

    Simple concepts.

---
+++ src/P1.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function P1() {
+  return (
+    <div className="container">
+        <div>
+        <img
+            src="logo192.png"
+            width="40px"
+        />
+        <h1 >Fun Facts about React</h1>
+        <ul >
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordab Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is Maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps.</li>
+        </ul>
+        </div>
+    </div>
+  );
+}
---
+++ src/P2.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function P2() {
+  return (
+    <div className="container">
+        <div>
+        <header>
+            <nav>
+            <img src="logo192.png" className="" width={50} />
+            </nav>
+        </header>
+        <h1 className="">Reason I am Exited to learn raect</h1>
+        <ol>
+            <li>It is a popular library</li>
+            <li>I'm more likely to get a job as a developer</li>
+        </ol>
+        <footer>
+            <small className="">&copy; Copyright 2023 - SarthakThapa</small>
+        </footer>
+        </div>
+    </div>
+  );
+}
