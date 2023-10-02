170eb95de2bd09baabdcdae41d6b881947a0a6c4
Sarthak Thapa <sarthakthapa634@gmail.com>
2023-10-02 22:00:26 +0545

    Simple bootstrap nav design.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import P1 from './P1';
+// import P1 from './P1';
 import P2 from './P2';
 
 function App() {
   return (
     <>
-      <P1/>
+      {/* <P1/> */}
       <P2/>
       
     </>
