a4459cbf74ec3c222bb59b6eece1e080ae8d9ad8
Sarthak Thapa <sarthakthapa634@gmail.com>
2023-10-10 22:21:59 +0545

    Project-2 AirBnb Starting.

---
+++ src/P2AirBnb.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './P2AirBnb.css'
+import "./images/airbnb.png";
+
+export default function P2AirBnb() {
+  return (
+    <div>
+      <nav>
+        <img src="airbnb.png" />
+        <ul>
+          <li>Home</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
